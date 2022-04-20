973c0a6e352fe2bd4294724f882e90eaf5515259
Ash Tewari <ash.tewari.name@gmail.com>
2022-04-19 22:05:31 -0400

    updated contracts

--- migrations/2_deploy_coin.js
+++ migrations/2_deploy_coin.js
@@ -4,5 +4,9 @@ const KhelCoinSale = artifacts.require("./KhelCoinSale");
 module.exports = async function (deployer) {
     let addr = await web3.eth.getAccounts();
     await deployer.deploy(KhelCoin);
-    await deployer.deploy(KhelCoinSale, 1, addr[0], KhelCoin.address);      
+    await deployer.deploy(KhelCoinSale, 10000, addr[0], KhelCoin.address);      
+    
+    let khel = await KhelCoin.deployed();
+
+    khel.transfer(KhelCoinSale.address, web3.utils.toWei('5000000'));
 };

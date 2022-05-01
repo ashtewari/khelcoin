01c25d86aa02178707cbafe489ebf1c75b0c601a
Ash Tewari <ash.tewari.name@gmail.com>
2022-04-30 21:50:07 -0400

    test 1

--- test/KhelCoin.tests.js
+++ test/KhelCoin.tests.js
@@ -3,8 +3,8 @@ var KhelCoin = artifacts.require("./KhelCoin.sol");
 contract('KhelCoin', async accounts => {
   
   it('initializes the contract with correct values', async () => {
-
-    
-
+    const instance = await KhelCoin.deployed();
+    const name = await instance.name();
+    assert.equal(name, 'KHEL Coin', 'token has correct name')
   });
 });

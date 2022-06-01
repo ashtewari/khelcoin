bc35144989a523ea5cb4764c5719ccfe8bc6151e
Ash Tewari <ash.tewari.name@gmail.com>
2022-05-31 23:11:50 -0400

    deployed to optimistic kovan

--- truffle-config.js
+++ truffle-config.js
@@ -18,6 +18,10 @@
  *
  */
 
+ require('dotenv').config();
+ const kovanMnemonic = process.env["KOVAN_MNEMONIC"];
+ const infuraKey = process.env["INFURA_PROJECT_ID"];
+
  const HDWalletProvider = require('@truffle/hdwallet-provider');
  const fs = require('fs');
  const mnemonic = fs.readFileSync(".secret").toString().trim();
@@ -58,21 +62,34 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     rinkeby: {
-    provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/45447924877b4ec8a54912178739c929'),
+    provider: () => new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io/v3/' + infuraKey),
     network_id: 4,       // Rinkeby's id
     gas: 5500000,        // Rinkeby has a lower block limit than mainnet
     confirmations: 2,    // # of confs to wait between deployments. (default: 0)
     timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
     skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
     },
+    kovan: {
+      provider: () => new HDWalletProvider(mnemonic, 'https://kovan.infura.io/v3/' + infuraKey, 3, 1),
+      network_id: 42,       // Kovan's id
+      gas: 5500000,        
+      confirmations: 2,    // # of confs to wait between deployments. (default: 0)
+      timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
+      skipDryRun: false     // Skip dry run before migrations? (default: false for public nets )
+      },    
+    optimistic_kovan: {
+      network_id: 69,
+      provider: function() {
+        return new HDWalletProvider(kovanMnemonic, "https://optimism-kovan.infura.io/v3/"+ infuraKey, 3, 1);
+      }
+    },   
     // Useful for private networks
     // private: {
     // provider: () => new HDWalletProvider(mnemonic, `https://network.io`),
     // network_id: 2111,   // This network is yours, in the cloud.
     // production: true    // Treats this network as if it was a public net. (default: false)
-    // }
+    // },    
   },
-
   // Set default mocha options here, use special reporters etc.
   mocha: {
     // timeout: 100000

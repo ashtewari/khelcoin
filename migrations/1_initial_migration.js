b136632a252ef7e4cd56890fd3db5b2bd4303c46
Ash Tewari <ash.tewari.name@gmail.com>
2022-07-31 11:16:50 -0400

    updated migrations

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,7 @@
 const Migrations = artifacts.require("Migrations");
 
-module.exports = function (deployer) {
-  deployer.deploy(Migrations, {overwrite: true});
+module.exports = async function (deployer) {
+  web3.eth.transactionPollingTimeout = 5400;
+  let accounts = await web3.eth.getAccounts();
+  deployer.deploy(Migrations, {from: accounts[1], overwrite: false});
 };
--- migrations/3_deploy_ico.js
+++ migrations/3_deploy_ico.js
@@ -1,7 +1,8 @@
 const KhelCoin = artifacts.require("./KhelCoin");
+const KhelCoinSale = artifacts.require("./KhelCoinSale");
 
 module.exports = async function (deployer) {
     web3.eth.transactionPollingTimeout = 5400;
     let accounts = await web3.eth.getAccounts();  
-    await deployer.deploy(KhelCoin, {from: accounts[1], overwrite: false});   
+    await deployer.deploy(KhelCoinSale, 10000, accounts[1], KhelCoin.address, {from: accounts[1], overwrite: false});
 };
